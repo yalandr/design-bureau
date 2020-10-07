f370427b33bf9bb4b49a03072c47934e32f3eca4
yalandr <52127093+yalandr@users.noreply.github.com>
2020-10-07 22:49:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Intro Slider
+
+$(document).ready(function(){
+		  $(".owl-carousel").owlCarousel({
+		  	items: 1,
+		  	loop: true,
+		  	autoplay: true,
+		  	autoplayTimeout: 4000,
+		  	animateOut: 'fadeOut'
+		  });
+		});
