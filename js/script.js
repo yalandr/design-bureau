079b48c9e43ee7ce9b7f2aafc0a19980269d04b8
yalandr <52127093+yalandr@users.noreply.github.com>
2020-11-03 23:28:09 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,57 +1,31 @@
 // Intro Slider
 
-$(document).ready(function(){
-		  $(".owl-carousel").owlCarousel({
-		  	items: 1,
-		  	loop: true,
-		  	autoplay: true,
-		  	autoplayTimeout: 4000,
-		  	animateOut: 'fadeOut'
-		  });
-		});
+$(document).ready(function () {
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		animateOut: 'fadeOut'
+	});
+});
 
 // Call Us Block
 
-const plusButton = document.querySelector('.plus');
-const closeButton = document.querySelector('.call-us-close');
-const callUsBlock = document.querySelector('.call-us-block');
-
-function callUsBlockOpen () {
-	callUsBlock.style.left = '0';
-	callUsBlock.style.opacity = '1';
-}
-
-function callUsBlockClose () {
-	callUsBlock.style.left = '200vw';
-	callUsBlock.style.opacity = '0';
-}
-
-plusButton.onclick = callUsBlockOpen;
-closeButton.onclick = callUsBlockClose;
+$(document).ready(function () {
+	$('.plus').click(function (event) {
+		$('.call-us-block').addClass('active');
+	});
+	$('.call-us-close').click(function (event) {
+		$('.call-us-block').removeClass('active');
+	});
+});
 
 // Mobile Menu
 
-const mobileMenu = document.querySelector('.mobile-menu');
-const navItemMenu = document.querySelector('.nav-item-menu');
-const menuClose = document.querySelector('.menu-close');
-const header = document.querySelector('.header');
-
-function mobileMenuOpen () {
-	mobileMenu.style.opacity = '1';
-	mobileMenu.style.top = '4rem';
-	navItemMenu.style.display = 'none';
-	menuClose.style.display = 'block';
-	header.style.background = '#eee';
-}
-
-function mobileMenuClose () {
-	mobileMenu.style.opacity = '0';
-	mobileMenu.style.top = '-30rem';
-	navItemMenu.style.display = 'block';
-	navItemMenu.style.opacity = '1';
-	menuClose.style.display = 'none';
-	header.style.background = 'rgba(255, 255, 255, 0.98)';
-}
-
-navItemMenu.onclick = mobileMenuOpen;
-menuClose.onclick = mobileMenuClose;
+$(document).ready(function () {
+	$('.header-burger').click(function (event) {
+		$('.header-burger, .mobile-menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
